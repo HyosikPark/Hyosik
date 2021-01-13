46e1661e618fbd72807e1d94c9d8b7c5a565ab2d
hyogooz <gytlr01@naver.com>
2021-01-13 20:20:57 +0900

    site url update

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -18,7 +18,7 @@ const Project = () => {
           <article className='portfolio'>
             <a
               className='portfolio-img link'
-              href='https://k-community.vercel.app/'
+              href='https://biaskpop.com/'
               target='_blank'
               rel='noreferrer'
             >
@@ -33,7 +33,7 @@ const Project = () => {
               </div>
               <div className='project-title'>
                 <a
-                  href='https://k-community.vercel.app/'
+                  href='https://biaskpop.com/'
                   target='_blank'
                   rel='noreferrer'
                 >
